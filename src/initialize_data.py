22728947434c0d0bedbb0f5b7696bfbeaef37ed0
Luiz Lima <luizandremoyseslima@gmail.com>
2026-09-17 22:36:57 -0300

    Add typehints and docstring to create_wind_speed_data

--- src/initialize_data.py
+++ src/initialize_data.py
@@ -6,17 +6,34 @@ from dateutil.relativedelta import relativedelta
 
 
 def create_wind_speed_data(
-    years=20,
-    delay_h=16,
-    delay_m=4,
-    delay_y=3,
-    period_y=7,
-    amplitude_h=1,
-    amplitude_m=0.75,
-    amplitude_y=0.5,
-    mean_ws=6,
-    std_noise=0.5,
+    years: int = 20,
+    delay_h: int = 16,
+    delay_m: int = 4,
+    delay_y: int = 3,
+    period_y: int = 7,
+    amplitude_h: float = 1,
+    amplitude_m: float = 0.75,
+    amplitude_y: float = 0.5,
+    mean_ws: float = 6,
+    std_noise: float = 0.5,
 ):
+    """Creates an artificial wind speed timeseries including hourly, monthly and yearly seasonal variation.
+
+    Args:
+        years (int, optional): Number of years that the data spans. Defaults to 20.
+        delay_h (int, optional): Delay of the hourly seasonality, used to shift the peak during the day. Defaults to 16.
+        delay_m (int, optional): Delay of the monthly seasonality, used to shift the peak during the day. Defaults to 4.
+        delay_y (int, optional): Delay of the yearly seasonality, used to shift the peak during the day. Defaults to 3.
+        period_y (int, optional): Period of the yearly seasonality, in years. Defaults to 7.
+        amplitude_h (float, optional): Amplitude of the wind speed hourly component. Defaults to 1.
+        amplitude_m (float, optional): Amplitude of the wind speed monthly component. Defaults to 0.75.
+        amplitude_y (float, optional): Amplitude of the wind speed yearly component. Defaults to 0.5.
+        mean_ws (float, optional): Mean yearly wind speed. Defaults to 6.
+        std_noise (float, optional): Standard deviation of the random noise applied to the wind speed (mean=0). Defaults to 0.5.
+
+    Returns:
+        pd.DataFrame: DataFrame with the values of wind speed in m/s. Index 'TS', columns ['WS'].
+    """
     # Define the start and end dates and create an empty dataframe with the correct hourly index
     ts_start = "2000-01-01 00:00:00"
     ts_end = (
@@ -24,7 +41,8 @@ def create_wind_speed_data(
         + relativedelta(months=years * 12)
         - dt.timedelta(hours=1)
     ).strftime("%Y-%m-%d %H:%M:%S")
-    base_df = pd.DataFrame(index=(ts_start, ts_end)).asfreq("h")
+    base_df = pd.DataFrame(index=[ts_start, ts_end]).asfreq("h")
+    base_df.index.name = "TS"
 
     # Create individual seasonal components of the wind speed
     # Hourly variation, throughout the day
@@ -44,7 +62,10 @@ def create_wind_speed_data(
 
     # Sum all of the seasonality components and add random noise
     base_df["WS"] = (
-        base_df["ws_y"] * amplitude_y*2 + base_df["ws_d"] * amplitude_m*2 + base_df["ws_h"] * amplitude_h*2 + mean_ws
+        base_df["ws_y"] * amplitude_y * 2
+        + base_df["ws_d"] * amplitude_m * 2
+        + base_df["ws_h"] * amplitude_h * 2
+        + mean_ws
     )
     base_df["WS"] += np.random.normal(0, std_noise, (base_df[["WS"]].size))
     base_df["WS"] = base_df["WS"].clip(0)  # Wind speed can't be negative
